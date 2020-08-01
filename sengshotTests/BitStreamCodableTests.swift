e486ac1a40b38e9c837cd8f34def87217b157a03
adebayo <aeogunmuyiwa@gmail.com>
2020-08-01 15:11:02 -0700

    added BitStreamCodableTests

--- sengshotTests/BitStreamCodableTests.swift
+++ sengshotTests/BitStreamCodableTests.swift
@@ -20,5 +20,19 @@ extension Thing: BitStreamCodable {}
 
 class BitStreamCodableTests: XCTestCase {
 
+        func testBitStreamCodableDefaultImplementations() throws {
+            let input = Thing(num: 87, name: "This is a thing.", data: Data(repeating: 0x6f, count: 123))
+            var bitStream = WritableBitStream()
+            try input.encode(to: &bitStream)
+
+            let packedData = bitStream.packData()
+
+            var readableString = ReadableBitStream(data: packedData)
+
+            let output = try Thing(from: &readableString)
+
+            XCTAssertEqual(input, output)
+        }
+    
 
 }
